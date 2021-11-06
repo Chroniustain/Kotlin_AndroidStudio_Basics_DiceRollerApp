82303c0a637088da2c80fd75cc83a4a34af4e04d
Chronis Rigas <chronisrigas@gmail.com>
2021-11-07 00:25:04 +0200

    1st commit of the completed roll dice application (including images of the dice).

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+/** This activity lets the user to roll a virtual dice and see the result in his/here screen **/
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Good Luck! Dices Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+        // Do a dice roll when the app starts
+        rollDice()
+    }
+
+    /** This function will set the number of sides on our dice and when roll will be excecuted will show
+    a dice image with the result.**/
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // We are using a when condition into a variable to check what our roll function rolled and then asign
+        // the proper image for our dice.
+        val drawableResourse = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResourse)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
+    }
+}
+
+/** This is our class-blueprint of our 1st dice **/
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
